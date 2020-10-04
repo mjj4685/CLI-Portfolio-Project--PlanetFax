dadee80f653f54e170113d4b7de794b647ef9de5
MJ Johnson <mjohnson4685@gmail.com>
2020-10-04 14:49:18 -0700

    created CatFaxCLI class

---
+++ catfactz_cli.rb
@@ -0,0 +1,5 @@
+
+
+require_relative '../lib/catfactz_cli.rb'
+require_relative '../lib/api.rb'
+
---
+++ cli.rb
@@ -0,0 +1,20 @@
+class CatFaxCli
+
+    def user_input
+        puts "Welcome, friend of feline!"
+        puts "To see cat factz, enter 'meow'"
+        puts "To exit the CatFax, enter 'bye bye meow'"
+
+        
+        
+    end
+
+    def 
+        
+    end
+
+    
+
+
+end
+
