eb7d7523ee0dc189560b6d2a6a36d4dda95b0f8a
MJ Johnson <mjohnson4685@gmail.com>
2020-10-04 15:44:07 -0700

    Added module CatFactzCLI

--- lib/catfactz_cli.rb
+++
@@ -1,5 +0,0 @@
-
-
-require_relative '../lib/catfactz_cli.rb'
-require_relative '../lib/api.rb'
-
---
+++ lib/catfactz_cli/catfactz_cli.rb
@@ -0,0 +1,12 @@
+
+require_relative "./catfactz_cli/version"
+
+module CatFactzCLI
+    class Error < StandardError; end
+
+
+end
+
+puts "environment file"
+
+
