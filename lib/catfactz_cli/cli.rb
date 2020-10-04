ad521bdb69c73f25d4ec5fb4ba6c77dd97cc309f
MJ Johnson <mjohnson4685@gmail.com>
2020-10-04 16:31:29 -0700

    created two new class methods in cli .rb

--- lib/catfactz_cli/cli.rb
+++ lib/catfactz_cli/cli.rb
@@ -3,27 +3,39 @@ class CatFaxCLI::CLI
     def call
         puts "Welcome, friend of feline!"
         puts "To see catfax, enter 'meow'"
-        puts "To exit catfax, enter 'bye bye meow"
+        puts "To exit catfax, enter 'exit"
+        menu
 
     end
 
     def menu
         input = gets.strip.downcase
 
-        if input == "list"
-            puts "list"
+        if input == "meow"
+            puts "Meow there, what you would like to know?"
 
-        elsif
+        elsif input == "exit"
+            puts "bye bye meow"
 
         else
-            
-
+            invalid_entry
         end
 
 
 
     end
 
+    def catfactz_list
+        
+
+    end
+
+
+    def invalid_entry
+        puts "Invalid entry, try again"
+        menu
+
+    end
 
 
 
