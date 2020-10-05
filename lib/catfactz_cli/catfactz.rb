1bdcab6fb0c066e3d20e79421ba745d1c8a83234
MJ Johnson <mjohnson4685@gmail.com>
2020-10-04 17:44:41 -0700

    Added #save, #self.all, #initialize class methods

--- lib/catfactz_cli/catfactz.rb
+++ lib/catfactz_cli/catfactz.rb
@@ -3,6 +3,20 @@ class Catfactz
     attr_accessor :text, :type, :user
 
     @@all = []
+
+    def initialize(attr_hash)
+        attr_hash
+
+    
+
+
+    def save
+        @@all <<self
+    end
+
+    def self.all
+        @@all
+    end
     
 
 
